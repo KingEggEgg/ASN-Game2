80f413d9e0580d7db6cfc645a7328ccf4a62780c
KingEggEgg <112497074+KingEggEgg@users.noreply.github.com>
2026-08-19 10:19:21 +0800

    Add Firebase leaderboard config

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA0WDlHa_ea0Aq7HELvOaVyowdpHEq6uAg",
+  authDomain: "asn-rush-commercial-day.firebaseapp.com",
+  databaseURL: "https://asn-rush-commercial-day-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "asn-rush-commercial-day",
+  storageBucket: "asn-rush-commercial-day.firebasestorage.app",
+  messagingSenderId: "1060761920805",
+  appId: "1:1060761920805:web:1a153f3eaa3f42bb0726cc"
+};
+
+export const leaderboardPath = "asnRushScores";
